0888c67b4b7c0754fb206bd604778de6b16d176d
Rayyan Uddin <rayyan9290@gmail.com>
2023-04-11 23:16:15 +0530

    Updating ToDoList and making it more complete, still buggy though, Aaaaaagh!!! so many damn bugs

--- WebDev/ToDoList/app.js
+++ WebDev/ToDoList/app.js
@@ -1,17 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+//Requiring mongoose
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//Connecting to local database
+mongoose.connect("mongodb://127.0.0.1/todolistDB");
+
+const itemSchema = {
+    name:{
+        type: String,
+        required: true
+    }
+
+}
+
+
+const Item = mongoose.model("Item", itemSchema);
+
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
+
+
+//New schema for custom lists
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+//Creating a new model for the custom lists
+const List = mongoose.model("List", listSchema);
+
+
+
+
 
 //Requiring a local module
 const day = require(__dirname + "/date.js");
-
-
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,34 +59,87 @@ app.listen(3000, function(){
 });
 
 //Todo list items log
-var items = [ 'Workout', 'Fajr', 'Breakfast' ];
-var workItems = [];
+
+
 
 
 app.get("/", function(req, res){
 
-    let date = day.getDate();
-    res.render("list", {ListTitle: date, newListItemz: items});
+    Item.find({}).then((foundItems) => {
 
+        let date = day.getDate();
+
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems).then(() => {
+                console.log("Successfully inserted default items using mongoDB");
+            }).catch((err) => {
+                console.log(err);
+            })
+
+            res.redirect("/");
+        }
+
+        else{
+            res.render("list", { ListTitle: date, newListItemz: foundItems });
+        }
+
+       
+
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    
 });
 
 
 app.post("/", function(req, res){
 
-    let item = req.body.newItem;
-    console.log(req.body.button);
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
 
-    //The list name is passed in the form of a hidden input which detects from where the post request is coming from
-    if(req.body.button == "Work") {
-        
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else {
-        items.push(item);
-        res.redirect("/");
-    }
+    item.save().then(() =>{
+        console.log("Successfully added new item");
+    })
+    res.redirect("/");
 });
+
+
+app.post("/delete", function(req, res){
+
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId).then(() => {
+        console.log("Successfully deleted checked item");
+        res.redirect("/");
+
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+
+
+app.get("/:customListName", function(req, res){
+
+    const customListName = req.params.customListName;
+
+    const list = new List({
+        name: customListName,
+        items: defaultItems
+    });
+
+    list.save().then(() => {
+        console.log("Successfully added new list");
+
+
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
+
 
 
 app.get("/work", function(req, res){
