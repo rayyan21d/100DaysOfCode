dab99cced058afaed45c9dcee347d16915afead1
rayyan21d <rayyan9290@gmail.com>
2023-05-19 16:21:39 +0530

    Hooks-useState()

--- WebDev/React/IntroToHooks/Intro/src/components/App.jsx
+++ WebDev/React/IntroToHooks/Intro/src/components/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  //Destructuring the useState return array
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- WebDev/React/IntroToHooks/Intro/src/index.js
+++ WebDev/React/IntroToHooks/Intro/src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-var count = 0;
-
-function clickIf() {
-  count++;
-  console.log(count);
-}
-
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button onClick={clickIf}>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
